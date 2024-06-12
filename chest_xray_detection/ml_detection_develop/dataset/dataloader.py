d8524a2e240063c95a9cc7336d0c34a1fb5f2976
Ubuntu <ubuntu@ip-172-31-7-118.eu-west-3.compute.internal>
2024-06-12 16:19:41 +0000

    FEAT: object detection dataset and dataloader

---
+++ chest_xray_detection/ml_detection_develop/dataset/dataloader.py
@@ -0,0 +1,119 @@
+from pathlib import Path
+from typing import Optional
+import pandas as pd
+import torch
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MultiLabelBinarizer
+from torch.utils.data import DataLoader
+from torchvision.transforms.v2 import Compose
+
+from chest_xray_detection.ml_detection_develop.configs.settings import DATA_PATH
+from chest_xray_detection.ml_detection_develop.dataset.detection_dataset import (
+    DetectionDataset,
+)
+
+
+def get_train_val_dataloaders(
+    annotation_filepath: str | Path,
+    batch_size: int,
+    train_transforms: Optional[Compose],
+    val_transforms: Optional[Compose],
+    val_size: float = 0.25,
+    num_workers: int = 4,
+) -> tuple[DataLoader, DataLoader, dict]:
+    """
+    Get training and validation data loaders from a stratified train/val split.
+
+    Args:
+        annotation_filepath (str | Path): Path to the CSV file containing annotations.
+        batch_size (int): Batch size for DataLoader.
+        train_transforms (Optional[Compose]): Transformations to apply to training data.
+        val_transforms (Optional[Compose]): Transformations to apply to validation data.
+        val_size (float, optional): Percentage of data to use for validation. Defaults to 0.25.
+        num_workers (int, optional): Number of subprocesses to use for data loading. Defaults to 4.
+
+    Returns:
+        tuple[DataLoader, DataLoader, dict]: Tuple containing train DataLoader, validation DataLoader,
+                                             and dictionary representing the distribution of labels in the training set.
+    """
+    df_annotation = pd.read_csv(annotation_filepath)
+    list_images = [
+        DATA_PATH / i for i in df_annotation["Image Index"].unique().tolist()
+    ]
+    labels = [
+        df_annotation[df_annotation["Image Index"] == idx]["Finding Label"].values
+        for idx in df_annotation["Image Index"].unique()
+    ]
+
+    mlb = MultiLabelBinarizer()
+    one_hot_labels = mlb.fit_transform(labels)
+
+    train_images, val_images, train_labels, val_labels = train_test_split(
+        list_images,
+        one_hot_labels,
+        test_size=val_size,
+        random_state=42,
+        stratify=None,
+    )
+
+    train_loader = get_single_dataloader(
+        images_list=train_images,
+        annotation_filepath=annotation_filepath,
+        transforms=train_transforms,
+        batch_size=batch_size,
+        num_workers=num_workers,
+    )
+
+    val_loader = get_single_dataloader(
+        images_list=val_images,
+        annotation_filepath=annotation_filepath,
+        transforms=val_transforms,
+        batch_size=batch_size,
+        num_workers=num_workers,
+    )
+
+    label_distribution = {
+        phase: {
+            idx: sum(labels[:, idx]) / len(labels) for idx in range(labels.shape[1])
+        }
+        for phase, labels in zip(["training", "validation"], [train_labels, val_labels])
+    }
+
+    return train_loader, val_loader, label_distribution
+
+
+def get_single_dataloader(
+    images_list: list[Path],
+    annotation_filepath: str | Path,
+    transforms: Optional[Compose],
+    batch_size: int,
+    num_workers: int = 4,
+) -> DataLoader:
+    """
+    Get a single DataLoader from image paths and annotations file.
+
+    Args:
+        images_list (list[Path]): List of image file paths.
+        annotation_filepath (str | Path): Path to the CSV file containing annotations.
+        transforms (Optional[Compose]): Transformations to apply to data.
+        batch_size (int): Batch size for DataLoader.
+        num_workers (int, optional): Number of subprocesses to use for data loading. Defaults to 4.
+
+    Returns:
+        DataLoader: DataLoader for the dataset.
+    """
+    dataset = DetectionDataset(
+        images_list=images_list,
+        annotation_filepath=annotation_filepath,
+        transforms=transforms,
+    )
+
+    dataloader = DataLoader(
+        dataset=dataset,
+        batch_size=batch_size,
+        num_workers=num_workers,
+        pin_memory=torch.cuda.is_available(),
+        collate_fn=lambda batch: tuple(zip(*batch)),
+    )
+
+    return dataloader
---
+++ chest_xray_detection/ml_detection_develop/dataset/detection_dataset.py
@@ -0,0 +1,149 @@
+from pathlib import Path
+from typing import Optional
+
+import numpy as np
+import pandas as pd
+import torch
+import torchvision.transforms.functional as F
+from torch.utils.data import Dataset
+from torchvision import tv_tensors
+from torchvision.io import read_image
+from torchvision.transforms import Compose
+
+from chest_xray_detection.ml_detection_develop.configs.settings import (
+    LABEL_MAPPING_DICT,
+)
+
+
+class DetectionDataset(Dataset):
+    """
+    Custom dataset class for object detection tasks.
+    """
+
+    def __init__(
+        self,
+        images_list: list[Path],
+        annotation_filepath: str | Path,
+        transforms: Optional[Compose] = None,
+    ) -> None:
+        """
+        Initialize the DetectionDataset.
+
+        Args:
+            images_list (list[Path]): List of image file paths.
+            annotation_filepath (str | Path): Path to the CSV file containing annotations.
+            transforms (Optional[Compose]): Transformations to be applied to the images.
+        """
+        self.images_list = images_list
+        self.df_annotation = pd.read_csv(annotation_filepath)
+        self.transforms = transforms
+
+    def __len__(self) -> int:
+        """
+        Return the total number of samples in the dataset.
+        """
+        return len(self.images_list)
+
+    def __getitem__(self, idx: int) -> tuple[torch.Tensor, dict[str, torch.Tensor]]:
+        """
+        Retrieve the image and its corresponding target at the specified index.
+
+        Args:
+            idx (int): Index of the sample to retrieve.
+
+        Returns:
+            tuple[torch.Tensor, dict[str, torch.Tensor]]: tuple containing the image tensor and the target dictionary.
+        """
+        image_path = self.images_list[idx]
+        image = self._load_image(image_path)
+        boxes, labels = self._load_target(image_path.name, image)
+
+        target = {
+            "boxes": boxes,
+            "labels": labels,
+        }
+
+        if self.transforms is not None:
+            image, target = self.transforms(image, target)
+
+        return image, target
+
+    def _load_image(self, image_path: Path) -> torch.Tensor:
+        """
+        Load and preprocess the image.
+
+        Args:
+            image_path (Path): Path to the image file.
+
+        Returns:
+            torch.Tensor: Preprocessed image tensor.
+        """
+        image = read_image(str(image_path))
+
+        if image.shape[0] == 4:
+            image = image[:3, :, :]
+
+        if image.shape[0] == 3:
+            image = F.rgb_to_grayscale(image)
+
+        image = image.float() / 255.0
+
+        return tv_tensors.Image(image)
+
+    def _load_target(
+        self, image_name: str, image: torch.Tensor
+    ) -> tuple[torch.Tensor, torch.Tensor]:
+        """
+        Load and preprocess the target data for a given image.
+
+        Args:
+            image_name (str): Name of the image file.
+
+        Returns:
+            tuple[torch.Tensor, torch.Tensor]: tuple containing the bounding boxes and labels tensors.
+        """
+        boxes_df = self.df_annotation[self.df_annotation["Image Index"] == image_name][
+            ["Bbox [x", "y", "w", "h]"]
+        ]
+        boxes = self._process_boxes(boxes=boxes_df.values, image=image)
+
+        labels_df = self.df_annotation[self.df_annotation["Image Index"] == image_name][
+            ["Finding Label"]
+        ]
+        labels = self._process_labels(labels=labels_df.values.squeeze(-1))
+
+        return boxes, labels
+
+    def _process_boxes(self, boxes: np.ndarray, image: torch.Tensor) -> torch.Tensor:
+        """
+        Process the bounding boxes.
+
+        Args:
+            boxes (np.ndarray): Array of bounding boxes.
+
+        Returns:
+            torch.Tensor: Tensor of bounding boxes.
+        """
+        boxes = np.around(boxes, decimals=0)
+        boxes[:, 2] += boxes[:, 0]
+        boxes[:, 3] += boxes[:, 1]
+
+        boxes = torch.from_numpy(boxes).float()
+        return tv_tensors.BoundingBoxes(
+            boxes,
+            format=tv_tensors.BoundingBoxFormat.XYXY,
+            canvas_size=image.shape[-2:],
+        )
+
+    def _process_labels(self, labels: np.ndarray) -> torch.Tensor:
+        """
+        Process the labels.
+
+        Args:
+            labels (np.ndarray): Array of label strings.
+
+        Returns:
+            torch.Tensor: Tensor of label integers.
+        """
+        labels = [LABEL_MAPPING_DICT.mapping.encoder[label] for label in labels]
+        return torch.as_tensor(labels, dtype=torch.int64)
