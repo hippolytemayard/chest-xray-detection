a7144f6e219492b88e4a52ecbfebb69f72f81058
Ubuntu <ubuntu@ip-172-31-7-118.eu-west-3.compute.internal>
2024-06-12 15:29:06 +0000

    initializing setting file

---
+++ chest_xray_detection/ml_detection_develop/configs/settings.py
@@ -0,0 +1,5 @@
+from pathlib import Path
+
+DATA_PATH = Path("/home/ubuntu/data/images")
+LABEL_PATH = Path(__file__).parent / "label_encoder.yaml"
+ANNOTATION_PATH = Path("/home/ubuntu/data/BBox_List_2017.csv")
