1012523fadf10b4383f4f0298d5846f68f7f87bb
Ubuntu <ubuntu@ip-172-31-7-118.eu-west-3.compute.internal>
2024-06-23 12:41:42 +0000

    FEAT: test api

--- chest_xray_detection/ml_detection_api/application/api.py
+++ chest_xray_detection/ml_detection_api/application/api.py
@@ -1,13 +1,11 @@
 __version__ = "0.0.1"
 
-from fastapi import FastAPI, Request, UploadFile, File
-from fastapi.middleware.cors import CORSMiddleware
-from numpy import imag
+from fastapi import FastAPI, File, Request, UploadFile
 
-from chest_xray_detection.ml_detection_api.configs.settings import SERVER_NAME, logging
 from chest_xray_detection.ml_detection_api.application.load_models import (
     multi_class_detection_model,
 )
+from chest_xray_detection.ml_detection_api.configs.settings import SERVER_NAME, logging
 from chest_xray_detection.ml_detection_api.domain.inference.inference_pipeline import (
     run_xray_detection,
 )
@@ -30,7 +28,5 @@ def health_check():
 
 @app.post("/api/pathology-detection", response_model=list[BBoxPrediction])
 def chest_xray_analysis(file: UploadFile = File(...)) -> list[BBoxPrediction]:
-    print(file)
     image = file.file.read()
-
     return run_xray_detection(image=image, detection_model=multi_class_detection_model, debug=True)
